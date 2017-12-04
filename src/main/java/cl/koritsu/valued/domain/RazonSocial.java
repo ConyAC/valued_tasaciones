c73bb06857b22757b388245d4fb7034f6e0ef260
Constanza Aravena Caro <caravena@mbienes.cl>
2017-12-03 21:07:53 -0300

    avance

---
+++ src/main/java/cl/koritsu/valued/domain/RazonSocial.java
@@ -0,0 +1,62 @@
+package cl.koritsu.valued.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="razon_social")
+public class RazonSocial {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	Long id;
+	String rut = "";
+	String nombre = "";
+	String direccion = "";
+	@JoinColumn(name="comunaId")
+	Comuna comuna;
+	@JoinColumn(name="regionId")
+	Region region;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getRut() {
+		return rut;
+	}
+	public void setRut(String rut) {
+		this.rut = rut;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getDireccion() {
+		return direccion;
+	}
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	public Comuna getComuna() {
+		return comuna;
+	}
+	public void setComuna(Comuna comuna) {
+		this.comuna = comuna;
+	}
+	public Region getRegion() {
+		return region;
+	}
+	public void setRegion(Region region) {
+		this.region = region;
+	}
+	
+}
