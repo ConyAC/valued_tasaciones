2fc2e2c87ff51a619c9ce1c6a257d49e85d972f5
caravena <caravena@CARAVENA.mbienes.cl>
2017-12-26 22:47:45 -0300

    clases y tipo de bien

--- src/main/java/cl/koritsu/valued/domain/enums/ClaseBien.java
+++ src/main/java/cl/koritsu/valued/domain/enums/ClaseBien.java
@@ -2,7 +2,11 @@ package cl.koritsu.valued.domain.enums;
 
 public enum ClaseBien {
 	
-	INMUEBLE("Inmueble"),MUEBLE("Mueble");
+	INMUEBLE("Inmueble"),MUEBLE("Mueble"),
+	BIENES_URBANO_HAB("Bienes Urbanos Habitacionales"),
+	OTROS_BIENES_URBANO("Otros Bienes Urbanos"),
+	BIEN_RAIZ_AGRICOLA("Bien Raíz Agrícola"),
+	VEHICULO_MAQUINARIA_EQUIPOS("Vehículo, Maquinarias y Equipos");
 	String label;
 	ClaseBien(String label){
 		this.label = label;
