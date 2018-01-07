48ec3aad8e847bd08ff3a695a535fa644b00e354
Constanza Aravena Caro <caravena@mbienes.cl>
2018-01-07 18:07:32 -0300

    avance tasaciones en proceso

--- src/main/java/cl/koritsu/valued/view/transactions/TransactionsView2.java
+++ src/main/java/cl/koritsu/valued/view/transactions/TransactionsView2.java
@@ -67,8 +67,10 @@ import com.vaadin.ui.Table.TableDragMode;
 import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.Window;
 import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.themes.ValoTheme;
+
 
 import cl.koritsu.valued.ValuedUI;
 import cl.koritsu.valued.component.MovieDetailsWindow;
@@ -124,48 +126,162 @@ public final class TransactionsView2 extends VerticalLayout implements View {
 
     	addComponent(buildToolbar());
 
-    	HorizontalLayout hl = new HorizontalLayout();
-    	//layout = mapa();
-    	hl.addComponent(mapa());
-    	//mapa();
-    	
-    	table = buildTable();
-    	hl.addComponent(table);
-    	addComponent(hl);
-    }
-
-    public Panel mapa() {
-    	Panel rootLayout = new Panel();
-        rootLayout.setSizeFull();
-        addComponent(rootLayout);        
-
-        googleMap = new GoogleMap(apiKey, null, "english");
-		//situamos, inicialmente, el mapa en Santiago.
-		googleMap.setCenter(new LatLon(-33.448779, -70.668551));
-		googleMap.setSizeFull();
-				
-		googleMap.setMinZoom(10);
-		googleMap.setMaxZoom(20);
+        googleMap = new GoogleMap(apiKey, null, null);
         googleMap.setZoom(10);
-
-        googleMap.addMarker(kakolaMarker);
-        googleMap.addMarker("DRAGGABLE: Paavo Nurmi Stadion", new LatLon(
-        		-35.6751,	-71.543), true, "VAADIN/1377279006_stadium.png");
-        googleMap.addMarker("NOT DRAGGABLE: Iso-Heikkilä", new LatLon(
-        		-33.5908,	-70.576), false, null);
-        googleMap.addMarker(maariaMarker);
+        googleMap.setSizeFull();
         googleMap.setMinZoom(4);
         googleMap.setMaxZoom(16);
 
-        addComponent(googleMap);
-        //setExpandRatio(googleMap, 1.0f);
-
-
-//        table = buildTable();
-//        rootLayout.addComponent(table);
-        return rootLayout;
+        Panel mapsPanel = new Panel();
+        mapsPanel.setSizeFull();
+        mapsPanel.setHeight("800px");
+        mapsPanel.setContent(googleMap);
+        addComponent(mapsPanel);
        
+        Window mapToolBox = new Window("Map Tool Box");
+        mapToolBox.setClosable(false);
+        mapToolBox.setResizable(false);
+        mapToolBox.setPosition(210, 220);
+        mapToolBox.setWidth("350px");
+        mapToolBox.setHeight("520px");
+        mapToolBox.addStyleName("mywindowstyle");
+        UI.getCurrent().addWindow(mapToolBox);
+
+        HorizontalLayout latlonLayout = new HorizontalLayout();
+        latlonLayout.setSpacing(true);
+        
+        TextField latitude = new TextField("Lat");
+        latitude.setWidth("100px");
+        latitude.setNullSettingAllowed(true);
+        latitude.setNullRepresentation("0.0");
+        
+        TextField longitude = new TextField("Long");
+        longitude.setWidth("100px");
+        longitude.setNullSettingAllowed(true);
+        longitude.setNullRepresentation("0.0");
+        
+        latlonLayout.addComponent(latitude);
+        latlonLayout.addComponent(longitude);
+        
+        HorizontalLayout infoLayout = new HorizontalLayout();
+        infoLayout.setSpacing(true);
+        
+        Label currentLat = new Label();
+        currentLat.setCaption("Current Latitude");
+        
+        Label currentLon = new Label();
+        currentLon.setCaption("Current Longitude");
+
+        infoLayout.addComponent(currentLat);
+        infoLayout.addComponent(currentLon);
+        
+        TextField markerName = new TextField("Marker Name");
+
+        Label latErrMsg = new Label();
+        latErrMsg.addStyleName("mylabelstyle");
+        Label lonErrMsg = new Label();
+        lonErrMsg.addStyleName("mylabelstyle");
+        
+//        Button.ClickListener addMarkerListener = new ClickListener() {
+//			
+//			@Override
+//			public void buttonClick(ClickEvent event) {
+//				String mName = markerName.getValue();
+//				if(mName.isEmpty()){
+//					mName = "Marker";
+//				}
+//				Double dLat = 0.0; 
+//				Double dLon = 0.0; 
+//				dLat = Double.valueOf(currentLat.getValue());
+//				dLon = Double.valueOf(currentLon.getValue());
+//
+//				GoogleMapMarker customMarker = new GoogleMapMarker(mName, new LatLon(dLat, dLon),true, null);
+//				googleMap.addMarker(customMarker);
+//			}
+//		};
+        
+        Button addMarker = new Button("Add Marker", FontAwesome.ANCHOR);
+  //      addMarker.addClickListener(addMarkerListener);
+        
+//        Button.ClickListener moveView = new ClickListener() {
+//			
+//			@Override
+//			public void buttonClick(ClickEvent event) {
+//				Boolean val = true;
+//				Double dLat = 0.0; 
+//				Double dLon = 0.0; 
+//
+//				try {
+//					dLat = Double.valueOf(latitude.getValue());
+//				} catch (Exception e) {
+//					val = false;
+//					latErrMsg.setValue("Latitude is not a valid number");
+//				}
+//				try {
+//					dLon = Double.valueOf(longitude.getValue());
+//				} catch (Exception e) {
+//					val = false;
+//					lonErrMsg.setValue("Longitude is not a valid number");
+//				}
+//				
+//				if(val){
+//					latErrMsg.setValue("");
+//					lonErrMsg.setValue("");
+//					if((dLat<= -85.0) || (dLat >= 85.0)){
+//						val = false;
+//						latErrMsg.setValue("Latitude must be between -85.0 and 85.0");
+//					}
+//					if((dLon<= -180.0) || (dLon >= 180.0)){
+//						val = false;
+//						lonErrMsg.setValue("Longitude  must be between -180.0 and 180.0");
+//					}
+//				}
+//				
+//				if(val){
+//					latErrMsg.setValue("");
+//					lonErrMsg.setValue("");
+//	                googleMap.setCenter(new LatLon(dLat, dLon));
+//	                googleMap.setZoom(12);
+//	                currentLat.setValue(latitude.getValue());
+//	                currentLon.setValue(longitude.getValue());
+//				}
+//			}
+//		};
+        
+        Button moveButton = new Button("Move", FontAwesome.BULLSEYE);
+       // moveButton.addClickListener(moveView);
+
+//        Button.ClickListener clearMarkerListener = new ClickListener() {
+//			
+//			@Override
+//			public void buttonClick(ClickEvent event) {
+//                googleMap.clearMarkers();
+//			}
+//		};
+
+		Button clearMarkersButton = new Button("Clear markers", FontAwesome.REMOVE);
+       // clearMarkersButton.addClickListener(clearMarkerListener);
+
+        Double newyorkLat = 40.7128;
+        Double newyorkLon = -74.0059;
+        googleMap.setCenter(new LatLon(40.7128, -74.0059));
+		GoogleMapMarker newyorkMarker = new GoogleMapMarker("New York", new LatLon(newyorkLat, newyorkLon),true, null);
+		googleMap.addMarker(newyorkMarker);
+		latitude.setValue(newyorkLat.toString());
+		longitude.setValue(newyorkLon.toString());
+		currentLat.setValue(latitude.getValue());
+		currentLon.setValue(longitude.getValue());
+
+        VerticalLayout toolLayout = new VerticalLayout();
+        toolLayout.setMargin(true);
+        toolLayout.setSpacing(true);
+        
+        table = buildTable();
+        toolLayout.addComponent(table);
+        mapToolBox.setContent(toolLayout);
     }
+
+  
     
     @Override
     public void detach() {
@@ -386,4 +502,170 @@ public final class TransactionsView2 extends VerticalLayout implements View {
     	List<SolicitudTasacion> solicitudes = service.getTasacionesByRegionAndComuna(comuna);
     	((BeanItemContainer<SolicitudTasacion>)table.getContainerDataSource()).addAll(solicitudes);
     }
+    
+    public void probando(VaadinRequest request) {
+        VerticalLayout rootLayout = new VerticalLayout();
+        rootLayout.setSizeFull();
+        addComponent(rootLayout);
+        
+
+        googleMap = new GoogleMap(apiKey, null, null);
+        googleMap.setZoom(10);
+        googleMap.setSizeFull();
+        googleMap.setMinZoom(4);
+        googleMap.setMaxZoom(16);
+
+        Panel mapsPanel = new Panel();
+        mapsPanel.setSizeFull();
+        mapsPanel.setContent(googleMap);
+        rootLayout.addComponent(mapsPanel);
+
+       
+        Window mapToolBox = new Window("Map Tool Box");
+        mapToolBox.setClosable(false);
+        mapToolBox.setResizable(false);
+        mapToolBox.setPosition(10, 100);
+        mapToolBox.setWidth("350px");
+        mapToolBox.setHeight("520px");
+        mapToolBox.addStyleName("mywindowstyle");
+        UI.getCurrent().addWindow(mapToolBox);
+
+        HorizontalLayout latlonLayout = new HorizontalLayout();
+        latlonLayout.setSpacing(true);
+        
+        TextField latitude = new TextField("Lat");
+        latitude.setWidth("100px");
+        latitude.setNullSettingAllowed(true);
+        latitude.setNullRepresentation("0.0");
+        
+        TextField longitude = new TextField("Long");
+        longitude.setWidth("100px");
+        longitude.setNullSettingAllowed(true);
+        longitude.setNullRepresentation("0.0");
+        
+        latlonLayout.addComponent(latitude);
+        latlonLayout.addComponent(longitude);
+        
+        HorizontalLayout infoLayout = new HorizontalLayout();
+        infoLayout.setSpacing(true);
+        
+        Label currentLat = new Label();
+        currentLat.setCaption("Current Latitude");
+        
+        Label currentLon = new Label();
+        currentLon.setCaption("Current Longitude");
+
+        infoLayout.addComponent(currentLat);
+        infoLayout.addComponent(currentLon);
+        
+        TextField markerName = new TextField("Marker Name");
+
+        Label latErrMsg = new Label();
+        latErrMsg.addStyleName("mylabelstyle");
+        Label lonErrMsg = new Label();
+        lonErrMsg.addStyleName("mylabelstyle");
+        
+//        Button.ClickListener addMarkerListener = new ClickListener() {
+//			
+//			@Override
+//			public void buttonClick(ClickEvent event) {
+//				String mName = markerName.getValue();
+//				if(mName.isEmpty()){
+//					mName = "Marker";
+//				}
+//				Double dLat = 0.0; 
+//				Double dLon = 0.0; 
+//				dLat = Double.valueOf(currentLat.getValue());
+//				dLon = Double.valueOf(currentLon.getValue());
+//
+//				GoogleMapMarker customMarker = new GoogleMapMarker(mName, new LatLon(dLat, dLon),true, null);
+//				googleMap.addMarker(customMarker);
+//			}
+//		};
+        
+        Button addMarker = new Button("Add Marker", FontAwesome.ANCHOR);
+  //      addMarker.addClickListener(addMarkerListener);
+        
+//        Button.ClickListener moveView = new ClickListener() {
+//			
+//			@Override
+//			public void buttonClick(ClickEvent event) {
+//				Boolean val = true;
+//				Double dLat = 0.0; 
+//				Double dLon = 0.0; 
+//
+//				try {
+//					dLat = Double.valueOf(latitude.getValue());
+//				} catch (Exception e) {
+//					val = false;
+//					latErrMsg.setValue("Latitude is not a valid number");
+//				}
+//				try {
+//					dLon = Double.valueOf(longitude.getValue());
+//				} catch (Exception e) {
+//					val = false;
+//					lonErrMsg.setValue("Longitude is not a valid number");
+//				}
+//				
+//				if(val){
+//					latErrMsg.setValue("");
+//					lonErrMsg.setValue("");
+//					if((dLat<= -85.0) || (dLat >= 85.0)){
+//						val = false;
+//						latErrMsg.setValue("Latitude must be between -85.0 and 85.0");
+//					}
+//					if((dLon<= -180.0) || (dLon >= 180.0)){
+//						val = false;
+//						lonErrMsg.setValue("Longitude  must be between -180.0 and 180.0");
+//					}
+//				}
+//				
+//				if(val){
+//					latErrMsg.setValue("");
+//					lonErrMsg.setValue("");
+//	                googleMap.setCenter(new LatLon(dLat, dLon));
+//	                googleMap.setZoom(12);
+//	                currentLat.setValue(latitude.getValue());
+//	                currentLon.setValue(longitude.getValue());
+//				}
+//			}
+//		};
+        
+        Button moveButton = new Button("Move", FontAwesome.BULLSEYE);
+       // moveButton.addClickListener(moveView);
+
+//        Button.ClickListener clearMarkerListener = new ClickListener() {
+//			
+//			@Override
+//			public void buttonClick(ClickEvent event) {
+//                googleMap.clearMarkers();
+//			}
+//		};
+
+		Button clearMarkersButton = new Button("Clear markers", FontAwesome.REMOVE);
+       // clearMarkersButton.addClickListener(clearMarkerListener);
+
+        Double newyorkLat = 40.7128;
+        Double newyorkLon = -74.0059;
+        googleMap.setCenter(new LatLon(40.7128, -74.0059));
+		GoogleMapMarker newyorkMarker = new GoogleMapMarker("New York", new LatLon(newyorkLat, newyorkLon),true, null);
+		googleMap.addMarker(newyorkMarker);
+		latitude.setValue(newyorkLat.toString());
+		longitude.setValue(newyorkLon.toString());
+		currentLat.setValue(latitude.getValue());
+		currentLon.setValue(longitude.getValue());
+
+        VerticalLayout toolLayout = new VerticalLayout();
+        toolLayout.setMargin(true);
+        toolLayout.setSpacing(true);
+        mapToolBox.setContent(toolLayout);
+        toolLayout.addComponent(clearMarkersButton);
+        toolLayout.addComponent(latlonLayout);
+        toolLayout.addComponent(moveButton);
+        toolLayout.addComponent(infoLayout);
+        toolLayout.addComponent(markerName);
+        toolLayout.addComponent(addMarker);
+        toolLayout.addComponent(latErrMsg);
+        toolLayout.addComponent(lonErrMsg);
+	}
 }
